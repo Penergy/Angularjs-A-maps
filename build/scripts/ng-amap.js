71494697f95431163f04d34e508a4ba234da5109
pengyc <pengyc@rc.inesa.com>
2016-09-06 16:48:49 +0900

    add main file of project

---
+++ build/scripts/ng-amap.js
@@ -0,0 +1,31 @@
+angular.module('ngAMap',[]);
+
+/**
+ * @ngdoc controller
+ * @name MapController
+ */
+
+(function() {
+    'use strict';
+    var Attr2MapOptions;
+
+    var __MapController = function(
+        $scope, $element, $attrs, $parse, _Attr2MapOptions_, NgAMap, NgAMapPool
+        ){
+    Attr2MapOptions = _Attr2MapOptions_;
+    var vm = this;
+
+    vm.mapOptions; /** @memberof __MapController */
+    vm.mapEvents; /** @memberof __MapController */
+    vm.eventListeners: /** @memberof __MapController*/
+
+    vm.addObject = function(groupName, obj){
+
+    };
+
+    vm.deleteObject = function(groupName, obj){
+    
+    };
+
+
+})
